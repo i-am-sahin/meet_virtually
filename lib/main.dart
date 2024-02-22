c396413d79df32b0592482295dc8758c60e9db51
Sahin Islam <sahinislam878@.com>
2024-02-23 00:44:32 +0530

    Create LoginPage And Create a custom ButtonWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meet_virtually/screens/login_sccreen.dart';
+import 'package:meet_virtually/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,19 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-     
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      title: 'Meet Virtually',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const Text('Hello,World'),
+      routes: {
+        '/login': (context) => const LoginScreen(),
+      },
+      home: const LoginScreen(),
     );
   }
 }
-
-
---
+++ lib/screens/login_sccreen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:meet_virtually/widgets/custom_button.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Start or join a meeting',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 38.0),
+            child: Image.asset('assets/images/onBoarding.jpg'),
+          ),
+          const CustomButton(
+            text: 'Login',
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
