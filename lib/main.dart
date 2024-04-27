b34b25721c91d781774e9ee2cc499c5a5077bb6c
Sahin Islam <sahinislam878@gmail.com>
2024-04-27 16:40:05 +0530

    addinf async in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:meet_virtually/screens/home_screen.dart';
 import 'package:meet_virtually/screens/login_sccreen.dart';
 import 'package:meet_virtually/utils/colors.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
